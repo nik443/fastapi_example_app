c0c134c1364c4cc068ee33894ddaa5287f305cee
nik443 <nikhram301@gmail.com>
2024-12-31 16:34:06 +1000

    creaete main settings

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 import uvicorn
 from fastapi import FastAPI
 
+from app.api import router as api_router
+from app.core.config import settings
+
 
 app = FastAPI()
+app.include_router(router=api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", reload=True, host="127.0.0.1", port=5000)
+    uvicorn.run(
+        app="main:app",
+        reload=True,
+        host=settings.run.host,
+        port=settings.run.port,
+    )
