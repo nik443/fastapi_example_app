465410d3c7cbdc1e1cf6693dfad842aae591b1e8
nik443 <nikhram301@gmail.com>
2025-01-01 12:56:01 +1000

    create db config

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PostgresDsn
 from pydantic_settings import BaseSettings
 
 
-class RunSettings(BaseModel):
+class RunConfig(BaseModel):
     host: str = "127.0.0.1"
     port: int = 8000
 
@@ -11,9 +11,18 @@ class ApiPrefix(BaseModel):
     prefix: str = "/api"
 
 
+class DatabaseConfig(BaseModel):
+    url: PostgresDsn
+    echo: bool = False
+    echo_pool: bool = False
+    max_overflow: int = 10
+    pool_size: int = 50
+
+
 class Settings(BaseSettings):
-    run: RunSettings = RunSettings()
+    run: RunConfig = RunConfig()
     api: ApiPrefix = ApiPrefix()
+    db: DatabaseConfig
 
 
 settings = Settings()
